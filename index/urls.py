7a25f112832e5cd7ecdec166e682d206278c44b8
sandeshsitaula <navaltajohan@gmail.com>
2024-05-19 11:10:30 +0545

    sucessfully extended to base

--- index/urls.py
+++ index/urls.py
@@ -3,5 +3,10 @@ from index import views
 
 
 urlpatterns=[
-    path('',views.index),
+    path('',views.index,name='index'),
+    path('company/',views.company,name='company'),
+    path('meet_the_team/',views.meet_the_team,name="meet_the_team"),
+    path('privacy_policy/',views.privacy_policy,name="privacy_policy"),
+    path('contact/',views.contact,name='contact'),
+    path('work/',views.work,name='work'),
 ]
--- index/views.py
+++ index/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+def company(request):
+    return render(request,'company.html')
+def meet_the_team(request):
+    return render(request,'meet_the_team.html')
+
+def privacy_policy(request):
+    return render(request,'privacy_policy.html')
+
+def work(request):
+    return render(request,'work.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
