646ce0eb3cbd06abb17d9e5ca3d3e6df6ca8996e
sandeshsitaula <navaltajohan@gmail.com>
2024-05-27 15:17:23 +0545

    updated meta tags in all pages

--- index/models.py
+++ index/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    full_name=models.CharField(max_length=50)
+    email=models.EmailField()
+    phone_number=models.CharField(max_length=15)
+    company=models.CharField(max_length=100,null=True,blank=True)
+    help=models.TextField()
--- index/views.py
+++ index/views.py
@@ -17,6 +17,13 @@ def work(request):
     return render(request,'work.html')
 
 def contact(request):
-    return render(request,'contact.html')
+    if request.method=="GET":
+        return render(request,'contact.html')
+    if request.method=="POST":
+        try:
+            print(request.body)
+        except Exception as e:
+            print(e)
+            return render(request,'contact.html',{"error":str(e)})
 def services(request):
     return render(request,'services.html')
