375edd8f54c84e4de163e8c58bb6f9cf8b81a4be
sandeshsitaula <navaltajohan@gmail.com>
2024-06-02 14:19:51 +0545

    have https in cannonical link

--- index/context_processors.py
+++ index/context_processors.py
@@ -1,4 +1,8 @@
+
 def canonical_url(request):
+    # Build the absolute URI and replace http with https
+    canonical_url = request.build_absolute_uri(request.path)
+    secure_canonical_url = canonical_url.replace('http://', 'https://')
     return {
-        'canonical_url': request.build_absolute_uri(request.path)
+        'canonical_url': secure_canonical_url
     }
